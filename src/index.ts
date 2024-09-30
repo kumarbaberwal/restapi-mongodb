92d32287f872c0dec7bb7f22ea888a11fd4a214d
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-01 00:51:41 +0530

    tasks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import router from "./routers/tasks";
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.port || 8080;
 
@@ -8,6 +10,8 @@ app.get('/', (req: Request, res: Response)=>{
     res.send('Server is sending response');
 });
 
+
+app.use('/tasks', router);
 app.listen(port, ()=>{
     console.log(`Server is running on port http://localhost:${port}`);
 });
