01ae3b065ad10468d5946b91aa26167d0dd21273
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-03 02:22:15 +0530

    getusers by mongoose

--- src/controllers/getusers.ts
+++ src/controllers/getusers.ts
@@ -1,19 +1,47 @@
-import { MongoClient } from "mongodb";
-import { Request, Response } from "express";    
+// import { MongoClient } from "mongodb";
+// import { Request, Response } from "express";    
 
-const url = "mongodb://localhost:27017/";
-
-
-export const  getusers = async(req: Request, res: Response) =>{
-    
-    const client = new MongoClient(url);
-
-    const db = await client.db('Auth');
-
-    const collection = await db.collection('users');
+// const url = "mongodb://localhost:27017/";
 
 
-    var result = await collection.find().toArray();
+// export const  getusers = async(req: Request, res: Response) =>{
 
-    res.json(result);
-};
+//     const client = new MongoClient(url);
+
+//     const db = await client.db('Auth');
+
+//     const collection = await db.collection('users');
+
+
+//     var result = await collection.find().toArray();
+
+//     res.json(result);
+// };
+
+
+import mongoose from "mongoose";
+
+import { Request, Response } from "express";
+
+const userSchema = new mongoose.Schema(
+    {
+        "username": { type: String },
+        "email": { type: String },
+        "password": { type: String }
+    }
+);
+
+
+export const getusers = async (req: Request, res: Response) => {
+    try {
+        const userModel = mongoose.model("users", userSchema);
+
+        var result = await userModel.find();
+
+        res.send(result);
+
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import mongoose, { mongo } from "mongoose";
 import router from "./routers/routes";
 
 
@@ -10,6 +11,17 @@ const port = process.env.port || 8080;
 
 app.get('/', (req: Request, res: Response)=>{
     res.send('Server is sending response');
+});
+
+const url = "mongodb://localhost:27017/Auth";
+mongoose.Promise = Promise;
+mongoose.connect(url);
+mongoose.connection.on("connected", ()=> {
+    console.log('Connection Established !!!');
+});
+
+mongoose.connection.on("error", (error: Error)=>{
+    console.log(error);
 });
 
 
